f76f5650087d4773c47faa46c52ba7d69c3f5e83
Zhen Zhang <izgzhen@gmail.com>
2019-03-17 10:56:49 -0700

    add scripts

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+wget https://github.com/xiaojunxu/SQLNet/raw/master/data.tar.bz2
+tar -xjvf data.tar.bz2
+conda create --name py27 python=2.7
+conda activate py27
+pip install -r requirements.txt
+bash download_glove.sh
+python extract_vocab.py
+
